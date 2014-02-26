0859df3ddbbd953f88adfc4146d5551fe72b776e
roxanarugina <roxana.rugina@gmail.com>
2014-02-26 11:32:10 +0100

    controller static_pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BicyclinkV1::Application.config.secret_key_base = '88f5d34fee2c30dc6ffd915448182b89b82ace71de46120dbdb294dae2c8642658cb7ad8de844818aad60ffb275eb4e3174f172eb04a02fcb8717e59ce1127d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BicyclinkV1::Application.config.session_store :cookie_store, key: '_bicyclink_V1_session'
